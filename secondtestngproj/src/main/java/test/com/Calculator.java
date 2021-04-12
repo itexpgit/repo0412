3dff733c117e4a0ce22aa2e1ee78a1b6ef8e448b
itexpgit <66497358+itexpgit@users.noreply.github.com>
2021-04-11 19:00:33 -0500

    Add files via upload

---
+++ secondtestngproj/src/main/java/test/com/Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+/**
+ *
+ * @author Uma
+ */
+public class Calculator {
+    public int sub(int x,int y){
+        return(x-y);
+        
+    }
+    public int div(int x,int y){
+        return(x/y);
+    }
+}
---
+++ secondtestngproj/src/test/java/test/com/Calculatortwo.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Uma
+ */
+public class Calculatortwo {
+    static Calculator c;
+    
+    private static WebDriver driver;
+    
+    public Calculatortwo() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        c = new Calculator();
+        System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+     @Test
+    public void testAdd() {
+        assertEquals(c.sub(2, 2), 0);
+    }
+      @Test
+    public void testIndeed() throws Exception {
+        driver.get("http://indeed.com");
+        Thread.sleep(1000);
+        driver.quit();
+    }
+
+}
